fc2e0f294287f86ecf383e7411d43f7c88682d23
Geovanny Valladares <geovannyvalladares94@gmail.com>
2023-10-12 13:56:40 +0200

    hotfix

--- frontend/src/app/products/vinyl/page.jsx
+++ frontend/src/app/products/vinyl/page.jsx
@@ -12,7 +12,7 @@ export default async function VinylView() {
     <main className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden">
       
       <header className='self-start px-4 mt-14'>
-        <h1 className='text-4xl font-bold'>Vynyl's</h1>
+        <h1 className='text-4xl font-bold'>Vinyls</h1>
         <p className='text-[#445058]'>
           You like listening to classic vinyl? We got you covered.
         </p>
