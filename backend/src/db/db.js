b52ad644e8804c595cb953af2a0b33fa44075483
Geovanny Valladares <geovannyvalladares94@gmail.com>
2024-04-01 17:57:19 +0200

    Fix: db postgre online

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,15 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: true
-//   }
-// })
-
-export const sequelize = new Sequelize(
-  'MusicShop', 'postgres', process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: true
   }
-);
+})
+
+// export const sequelize = new Sequelize(
+//   'MusicShop', 'postgres', process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres'
+//   }
+// );
